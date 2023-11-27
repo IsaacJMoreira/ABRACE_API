66f2f0278ee59670b84d972ce8c4220e6aa79872
Isaac J Moreira <120274547+IsaacJMoreira@users.noreply.github.com>
2023-11-27 20:08:08 -0300

    ADD: Rest of the CRUD methods for the adm routes CONTROLLER

--- abrace_api/src/adms/adms.controller.ts
+++ abrace_api/src/adms/adms.controller.ts
@@ -1,31 +1,141 @@
-import { Controller, Post, Body, InternalServerErrorException, ForbiddenException } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  InternalServerErrorException,
+  ForbiddenException,
+  Get,
+  NotFoundException,
+  Param,
+  Put,
+  Delete,
+} from '@nestjs/common';
 import { AdmService } from './adms.service';
+import { response } from 'express';
 
 @Controller('adms')
-export class AdmsController{
-    constructor(private readonly admService: AdmService){}
+export class AdmsController {
+  constructor(private readonly admService: AdmService) {}
 
-    @Post()
-    async addAdm( 
-        @Body('name') admName: string,
-        @Body('email') admEmail: string,
-        @Body('pass') admPass: string,
-        @Body('credenctial') admCredential: string
-    )
-    {
-        let generatedID;
-        try{
-        generatedID = await this.admService.createAdm(
-            admName,
-            admEmail,
-            admPass,
-            admCredential
-        );       
-        } catch(error){
-            throw new InternalServerErrorException('Mongo DB se cagou ao criar esse adm')
-        }
-        if(!generatedID) throw new ForbiddenException("Já tem aguém com esse email, carai!!!")
-        return {ID: generatedID};
+  @Post()
+  async add(
+    @Body('name') admName: string,
+    @Body('email') admEmail: string,
+    @Body('pass') admPass: string,
+    @Body('credenctial') admCredential: string,
+  ) {
+    let generatedID;
+    try {
+      generatedID = await this.admService.createAdm(
+        admName,
+        admEmail,
+        admPass,
+        admCredential,
+      );
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Mongo DB se cagou ao criar esse adm',
+      );
+    }
+    if (!generatedID)
+      throw new ForbiddenException('Já tem aguém com esse email, carai!!!');
+    return { ID: generatedID };
+  }
+
+  @Get()
+  async getAll() {
+    let allADMs;
+
+    try {
+      allADMs = this.admService.getAdms();
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Mongo DB se cagou ao pegar todos os adms',
+      );
     }
 
+    if (!allADMs) throw new NotFoundException('Num achei foi nada!');
+    return allADMs;
+  }
+
+  @Get(':id')
+  async getById(@Param('id') admID: string) {
+    let specificADM;
+
+    try {
+      specificADM = await this.admService.getADMByID(admID);
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Mongo DB de cagou a tentar encontrar um adm especifico',
+      );
+    }
+
+    if (!specificADM)
+      throw new NotFoundException('Não achei esse adm ai q vc falou');
+
+    return specificADM;
+  }
+
+  @Put(':id')
+  async updateADM_NoCredential(
+    @Param('id') admID: string,
+    @Body('name') newName: string,
+    @Body('pass') newPass: string,
+    @Body('email') newEmail: string,
+  ) {
+    let response;
+    try {
+      response = await this.admService.putADMByID_NoCredential(
+        admID,
+        newName,
+        newEmail,
+        newPass,
+      );
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'MongoDB se cagou ao tentar fzr esse update',
+      );
+    }
+    if (response == 2)
+      throw new ForbiddenException('Já existe alguém com esse E-MAIL');
+    return response._id;
+  }
+
+  @Put('promote/:id')
+  async updateCredential(
+    @Param('id') admID: string,
+    @Body('credential') newAdmCredential: string,
+  ) {
+    let response;
+    try {
+      response = await this.admService.changeCredential(
+        admID,
+        newAdmCredential,
+      );
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Se cagou ao tentar mudar essa credencial',
+      );
+    }
+
+    if (!response)
+      throw new NotFoundException('Num encontrei ngm pra promover');
+
+    return response._id;
+  }
+
+  @Delete(':id')
+  async deleteAdmByID(@Param('id') admID: string) {
+    let response;
+    try {
+      response = await this.admService.deleteById(admID);
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'mongoDB se cagou ao tentar deletar o ADM',
+      );
+    }
+    if (response.deletedCount == 0)
+      throw new NotFoundException('Ninguém assim pra ser deletado!');
+    return response;
+  }
 }
--- abrace_api/src/adms/adms.service.ts
+++ abrace_api/src/adms/adms.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Adm } from './adm.model';
 import { Model } from 'mongoose';
@@ -13,17 +13,80 @@ export class AdmService {
     pass: string,
     credential: string,
   ) {
+    const existes = await this.admModel.findOne({ email: email });
 
-    const existes = await this.admModel.findOne({email: email})
-
-    if(!existes){
-        const response = await new this.admModel({name, email, pass, credential}).save();
-        return response._id;
+    if (!existes) {
+      const response = await new this.admModel({
+        name,
+        email,
+        pass,
+        credential,
+      }).save();
+      return response._id;
     }
-    
-    return false;   
+
+    return false;
   }
 
+  async getAdms() {
+    const response = await this.admModel.find();
+
+    if (!response) return false;
+    return response;
+  }
+
+  async getADMByID(id: string) {
+    const response = await this.admModel.findById(id);
+    console.log(response);
+    if (!response) return false;
+    return response;
+  }
+
+  async putADMByID_NoCredential(
+    id: string,
+    name: string,
+    email: string,
+    pass: string,
+  ) {
+    const alreadyExistes = await this.admModel.find({ email: email }, '_id');
+
+    let isItself = 0;
+    alreadyExistes.forEach((element) => {
+      if (element._id == id) isItself = 1;
+    });
+    console.log('isItsenf', isItself);
+    console.log('alreadyExistes', alreadyExistes.length);
+
+    if (alreadyExistes.length > 0 && !(isItself == 1)) return 2;
+
+    const response = await this.admModel.findByIdAndUpdate(
+      { _id: id },
+      {
+        name,
+        email,
+        pass,
+      },
+      {
+        new: true,
+      },
+    );
+
+    return response;
+  }
+
+  async changeCredential(id: string, credential: string) {
+    const exists = await this.admModel.findByIdAndUpdate(
+      { _id: id },
+      { credential },
+      { new: true },
+    );
+    if (!exists) return false;
+    return exists;
+  }
+
+  async deleteById(id: string) {
+    const deleted = await this.admModel.deleteOne({ _id: id });
+    if (!deleted) return false;
+    return deleted;
+  }
 }
-
-
